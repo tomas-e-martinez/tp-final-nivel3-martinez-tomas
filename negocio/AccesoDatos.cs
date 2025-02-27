93416995099b0c09684cef370d15d7315e990e70
Tomás Martínez <tomas.emart@gmail.com>
2025-02-27 00:00:36 -0300

    cadena de conexion a web.config

--- negocio/AccesoDatos.cs
+++ negocio/AccesoDatos.cs
@@ -4,6 +4,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace negocio
 {
@@ -19,7 +20,7 @@ namespace negocio
 
         public AccesoDatos()
         {
-            conexion = new SqlConnection("server=.\\SQLEXPRESS; database=CATALOGO_WEB_DB; integrated security=true");
+            conexion = new SqlConnection(ConfigurationManager.AppSettings["cadenaConexion"]);
             comando = new SqlCommand();
         }
 
